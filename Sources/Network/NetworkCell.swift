7436fd0fc1c3734f89af7890f1d40ce40c1474cd
acumenrev <acumenvn@live.com>
2025-07-10 11:33:08 +0700

    Update network cell

--- Sources/Network/NetworkCell.swift
+++ Sources/Network/NetworkCell.swift
@@ -44,14 +44,15 @@ class NetworkCell: UITableViewCell {
                 }
             }
             
-            if let headers = httpModel?.requestHeaderFields, let graphQLName = headers["X-APOLLO-OPERATION-NAME"] as? String, !graphQLName.isEmpty {
-                let operationType = headers["X-APOLLO-OPERATION-TYPE"] as? String
-                requestUrlTextView.text += "GraphQL: \(operationType ?? "query") \(graphQLName) endpoint: \(httpModel?.url.absoluteString ?? "")"
-            }
-            
             //Request method
             if let method = httpModel?.method {
                 methodLabel.text = "[" + method + "]"
+            }
+            
+            if let headers = httpModel?.requestHeaderFields, let graphQLName = headers["X-APOLLO-OPERATION-NAME"] as? String, !graphQLName.isEmpty {
+                let operationType = headers["X-APOLLO-OPERATION-TYPE"] as? String
+                requestUrlTextView.text = "GraphQL: \(operationType ?? "query") \(graphQLName) endpoint: \(httpModel?.url.absoluteString ?? "")"
+                methodLabel.text = "GraphQL"
             }
             
             //Request time
